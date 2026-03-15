7e25bc304c0c097705d7a782e445679356e9e935
hoai <dtdinh@caothang.edu.vn>
2026-03-15 15:08:38 +0700

    cap nhat

--- SportsStoreWebApp/Program.cs
+++ SportsStoreWebApp/Program.cs
@@ -1,3 +1,29 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 Console.WriteLine("---Thuc hanh C# co ban---");
 
 List<SportsStoreWebApp.Models.Product> sampleProducts = new List<SportsStoreWebApp.Models.Product>
@@ -35,3 +61,7 @@ async Task SimulateDataFetchAsync()
 await SimulateDataFetchAsync(); // Cần `await` ở đây vì hàm Main của .NET 6+ đãlà async
 Console.WriteLine("--- Kết thúc thực hành C# cơ bản ---\n");
 // --- Kết thúc phần thực hành C# cơ bản ---
+
+
+
+app.Run();
